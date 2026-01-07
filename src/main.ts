20c6141a2ad64b786a9fc6e13f3abdf5d0a6c9f7
Sajid <sbicoredev@gmail.com>
2026-01-07 11:40:18 +0600

    feat: enabled api versioning, global prefix and trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { INestApplication } from "@nestjs/common";
+import {
+  INestApplication,
+  VERSION_NEUTRAL,
+  VersioningType,
+} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
@@ -11,6 +15,17 @@ async function bootstrap() {
 
   const config = app.get(ConfigService<Configurations, true>);
   const appConfig = config.get("app", { infer: true });
+
+  // configure trusted proxies for deployments behind load balancers or reverse proxies.
+  // this enables accurate client IP extraction from X-Forwarded-For headers,
+  app.set("trust proxy", appConfig.trustProxy);
+  // Set a global route prefix (e.g., '/api') for all controllers.
+  app.setGlobalPrefix(appConfig.globalPrefix);
+  // Enable URI-based API versioning (e.g., /api/v1/users).
+  app.enableVersioning({
+    defaultVersion: VERSION_NEUTRAL,
+    type: VersioningType.URI, // '1', ['1', '2'] or VERSION_NEUTRAL allows routes without an explicit version to be accessible.
+  });
 
   await app.listen(appConfig.port);
 
